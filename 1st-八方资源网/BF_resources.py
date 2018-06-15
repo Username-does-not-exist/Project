746a25c1e2c9b25ab50b85303d9b1c2d20737ab9
Username-does-not-exist <1432305526@qq.com>
2018-06-15 15:05:26 +0800

    Initial commit

---
+++ 1st-八方资源网/BF_resources.py
@@ -0,0 +1,267 @@
+import time
+from pymongo import MongoClient
+import requests
+from lxml import etree
+import sys
+sys.path.append('./')
+from UserAgentPool import UAPool
+from ProxyPool import IPool
+
+
+class Bfresources(object):
+    """
+    八方资源网外贸服饰商家信息抓取
+    """
+    def __init__(self):
+
+        # self.host = '127.0.0.1'
+        # self.port = 27017
+        # self.conn = MongoClient(host=self.port, port=self.port)
+        self.base_url = "https://www.b2b168.com/k-waimaofushi/l-{}.html"
+        self.headers = {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                    # "Accept-Encoding": "gzip, deflate",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "zh-CN,zh;q=0.9",
+                    "Connection": "keep-alive",
+                    "Upgrade-Insecure-Requests": "1",
+                    "User-Agent": UAPool().get(),
+                    "Host": "www.b2b168.com"
+        }
+
+    def get_proxy(self):
+        pro = IPool().get_proxy()
+        proxy = {
+            "http": "//" + pro
+        }
+        return proxy, pro
+
+    def parse_url(self):
+        """
+        构造待抓取的url
+        :return:
+        """
+        url_list = list()
+        for i in range(25):
+            url = self.base_url.format(i)
+            url_list.append(url)
+        return url_list
+
+    def get_detail_url(self, url):
+        """
+        获取商家相情url
+        :return:
+        """
+        response = requests.get(url=url)
+        page = response.text
+        html = etree.HTML(page)
+        detail_url_list = html.xpath('//*[@id="box02"]/div/div/ul/div/h1/a/@href')
+        return detail_url_list
+
+    def parse_data(self, url, proxy, pro):
+        """
+        获取数据
+        :return:
+        """
+        # try:
+        if "html" not in url:
+            pass
+        #         # response = requests.get(url=url, headers=self.headers, proxies = proxy)
+        #         # page = response.text
+        #         # html = etree.HTML(page)
+        #         #
+        #         # items = dict()
+        #         # try:
+        #         #     items["company_name"] = html.xpath('//*[@class="box-rightsidebar3"]/li/a[1]/text()')[0].\
+        #         #         replace(" ", "")
+        #         #
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items["contacts"] = html.xpath('//*[@class="box-rightsidebar3"]/li/a[2]/text()')[0].\
+        #         #         replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['phone_number'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[3]')[0].\
+        #         #         replace("\r\n\u3000\u3000电\u3000\u3000话： ", "").replace(" ", "")
+        #         #
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['fax'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[4]')[0].\
+        #         #         replace("\r\n\u3000\u3000传\u3000\u3000真： ", "").replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items["mobile_number"] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[5]')[0].\
+        #         #         replace("\r\n\u3000\u3000移动电话： ", "").replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['address'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[6]')[0].\
+        #         #         replace("\r\n\u3000\u3000地\u3000\u3000址： ", "").replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['post_number'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[7]')[0].\
+        #         #         replace("\r\n\u3000\u3000邮\u3000\u3000编： ", "").replace("\r\n\u3000\u3000\u3000\u3000邮件留言：", "").\
+        #         #         replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['messager'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[8]')[0].\
+        #         #         replace("\r\n\u3000\u3000Messager： ", "").replace("\u3000\u3000邮件留言：", "").replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['bf_tong'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[9]')[0].\
+        #         #         replace("\r\n\u3000\u3000八 方 通：", "").replace("\r\n\u3000\u3000Messager：", "").replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         #
+        #         # try:
+        #         #     items['company_url'] = html.xpath('//*[@class="box-rightsidebar3"]/li/a[5]/@href')[0].replace(" ", "")
+        #         # except Exception as e:
+        #         #     print(e)
+        #         # print(items)
+        #         # return items
+        #
+        else:
+            try:
+                response = requests.get(url=url, headers=self.headers, proxies=proxy, timeout=10)
+                i = 0
+                while i < 5:
+                    if response.status_code != 200:
+                        IPool().delete_proxy(pro)
+                        proxy, pro = self.get_proxy()
+                        response = requests.get(url=url, headers=self.headers, proxies=proxy, timeout=15)
+                        time.sleep(0.5)
+                    else:
+                        break
+                page = response.text
+                html = etree.HTML(page)
+
+                items = dict()
+                try:
+                    items["company_name"] = html.xpath('//*[@class="Cleft"]/ul[2]/text()')[0].replace(" ", "")
+                    if items['company_name'] == None:
+                        items['company_name'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items["contacts"] = html.xpath('//*[@class="codl"]/dd[3]/text()')[0].replace(" ", "")
+                    if items["contacts"] == None:
+                        items["contacts"] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['phone_number'] = html.xpath('//*[@class="codl"]/dd[2]/text()')[0].replace(" ", "")
+                    if items['phone_number'] == None:
+                        items['phone_number'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['fax'] = html.xpath('//*[@class="codl"]/dd[6]/text()')[0].replace(" ", "")
+                    if items['fax'] == None:
+                        items['fax'] = ''
+
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items["mobile_number"] = html.xpath('//*[@class="codl"]/dd[4]/text()')[0].replace(" ", "")
+                    if items["mobile_number"] == None:
+                        items["mobile_number"] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['address'] = html.xpath('//*[@class="codl"]/dd[1]/text()')[0].replace(" ", "")
+                    if items['address'] == None:
+                        items['address'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['post_number'] = html.xpath('//*[@class="codl"]/dd[5]/text()')[0].replace(" ", "")
+                    if items['post_number'] == None:
+                        items['post_number'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['messager'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[8]')[0].replace(" ", "")
+                    if items['messager'] == None:
+                        items['messager'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['bf_tong'] = html.xpath('//*[@class="box-rightsidebar3"]/li/text()[9]')[0].replace(" ", "")
+                    if items['bf_tong'] == None:
+                        items['bf_tong'] = ''
+                except Exception as e:
+                    print(e)
+
+                try:
+                    items['company_url'] = html.xpath('//*[@class="codl"]/dd[7]/text()')[0].replace(" ", "")
+                    if items['company_url'] == None:
+                        items['company_url'] = ''
+                except Exception as e:
+                    print(e)
+
+                return items
+            except Exception as e:
+                print(e)
+
+
+        # except Exception as e:
+        #     print(e)
+
+    def save_data(self, items):
+        """
+        数据存储
+        :return:
+        """
+
+    def run(self):
+        """
+        程序运行逻辑
+        :return:
+        """
+        contact_url_list = list()
+        url_list = self.parse_url()
+        for url in url_list:
+            detail_url_list = self.get_detail_url(url)
+            for url in detail_url_list:
+                if "com" in url and "html" in url:
+                    contact_url_list.append(url)
+                if "com" in url and "html" not in url:
+                    contact_url = url + "contact.aspx"
+                    contact_url_list.append(contact_url)
+
+        for url in contact_url_list:
+            print(url)
+            proxy, pro = self.get_proxy()
+            items = self.parse_data(url, proxy, pro)
+            time.sleep(0.2)
+            print(items)
+
+            # self.save_data(items)
+
+
+if __name__ == '__main__':
+    BF = Bfresources()
+    BF.run()
