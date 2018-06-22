f8c5d3db36d0acc3225be829e8ad01566bac644c
Username-does-not-exist <1432305526@qq.com>
2018-06-22 15:54:53 +0800

    代码结构完善

--- FeJiu/FeJiu.py
+++ FeJiu/FeJiu.py
@@ -1,5 +1,7 @@
 import time
 import random
+
+import redis
 import requests
 from lxml import etree
 from selenium import webdriver
@@ -20,7 +22,9 @@ class FeJiu(object):
         self.start_url = "http://www.feijiu.net/FeiZhi/g1/"
         self.Host = "127.0.0.1"
         self.Port = 27017
+        self.rPort = 6379
         self.conn = MongoClient(host=self.Host, port=self.Port)
+        self.rConn = redis.Redis(host=self.Host, port=self.rPort)
 
     def get_headers(self):
         headers = {
@@ -77,18 +81,21 @@ class FeJiu(object):
         """
         try:
             response = requests.get(url=url, proxies=proxy, headers=headers, timeout=10)
+            print(response.status_code)
             if response.status_code == 200:
                 page = response.text
                 html = etree.HTML(page)
                 url_list = html.xpath('//*[@id="list_item"]/div[1]/div/div/div[1]/h1/a/@href')
-                for url in url_list:
-                    print("<............{}..............>".format(url))
-                    return url
-                next_page_url = html.xpath('//*[@id="AspNetPager1"]/a[last()-1]/@href')
-                if next_page_url != None:
-                    return next_page_url
-                else:
-                    pass
+                next_page_url = html.xpath('//*[@id="AspNetPager1"]/a[last()-1]/@href')[0]
+                try:
+                    element = html.xpath('//*[@id="AspNetPager1"]/a[last()-1]/@disabled')[0]
+                    if element != "disabled":
+                        element = None
+                        return url_list, next_page_url, element
+                    else:
+                        return url_list, next_page_url, element
+                except Exception as e:
+                    print(e)
             else:
                 pass
         except Exception as e:
@@ -111,22 +118,24 @@ class FeJiu(object):
         except Exception as e:
             print(e)
 
-    def save_data(self, data):
+    def save_data(self, url):
         """
         报讯数据
         :param data:
         :return:
         """
-        try:
-            db = self.conn.FeJiu
-            col = db.fejiu
-            col.insert(data)
-            count = col.count()
-            print(data)
-            print("<|---------------=================----------------|>")
-            print("当前已抓取{}条数据".format(count))
-        except Exception as e:
-            print(e)
+        # try:
+        #     db = self.conn.FeJiu
+        #     col = db.fejiu
+        #     col.insert(data)
+        #     count = col.count()
+        #     print(data)
+        #     print("<|---------------=================----------------|>")
+        #     print("当前已抓取{}条数据".format(count))
+        # except Exception as e:
+        #     print(e)
+
+        self.rConn.hset("FeJuiURL", url, 1)
 
     # def __del__(self):
     #     self.driver.close()
@@ -141,42 +150,49 @@ class FeJiu(object):
         pro, proxy = self.get_proxy()
         try:
             distract_url_list = self.get_distract_url(headers, proxy)
-            print(distract_url_list)
-            # 获取每一个省份的各个商家的详情页url
+
             detail_url_list = list()
+            next_page_url_List = list()
+
+            # 获取每一个省份的各个商家的详情页url
             for url in distract_url_list:
-                while True:
-                    print("-------------{}--------------".format(url))
-                    url, next_page_url = self.parse_detail_url(url, headers, proxy)
-                    detail_url_list.append(url)
+                try:
+                    print("------------------------{}------------------------".format(url))
+                    url_list, next_page_url, element = self.parse_detail_url(url, headers, proxy)
                     time.sleep(6)
-                    if next_page_url:
-                        url, next_page_url = self.parse_detail_url(url, headers, proxy)
+                    for url in url_list:
+                        detail_url_list.append(url)
+                        print("-------------{}--------------".format(url))
+                    if element == "disabled":
+                        pass
                     else:
-                        break
+                        # 获取下一页
+                        next_page_url_List.append(next_page_url)
+                        while True:
+                            for url in next_page_url_List:
+                                try:
+                                    print("``````````````````next_page``````````````````")
+                                    url_list, next_page_url, element = self.parse_detail_url(url, headers, proxy)
+                                    time.sleep(6)
+                                    for url in url_list:
+                                        detail_url_list.append(url)
+                                        print("-------------{}--------------".format(url))
+                                    if element == "disabled":
+                                        pass
+                                    next_page_url_List.append(next_page_url)
+                                except Exception as e:
+                                    print(e)
+                            break
 
+                except Exception as e:
+                    print(e)
 
+            for url in detail_url_list:
+                # data = self.parse_data(url, headers, proxy)
+                self.save_data(url)
 
-
-                # while True:
-                #     if next_page_url:
-                #         url_list, next_page_url = self.parse_detail_url(next_page_url, headers, proxy)
-                #         for url in url_list:
-                #             detail_url_list.append(url)
-                #             print(detail_url_list)
-                #             print(len(detail_url_list))
-                #         time.sleep(5)
-                #     else:
-                #         for url in url_list:
-                #             detail_url_list.append(url)
-                #         break
-            print(detail_url_list)
         except Exception as e:
-            print(e)
-        # 提取数据
-        # for url in detail_url_list:
-        #     data = self.parse_data(url, headers, proxy)
-        #     self.save_data(data)
+            print("***********{}**************".format(e))
 
 
 if __name__ == '__main__':
