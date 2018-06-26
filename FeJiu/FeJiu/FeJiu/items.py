49727d3f5bdb99037f54feffda1a14effc86b176
Username-does-not-exist <1432305526@qq.com>
2018-06-26 15:07:15 +0800

    废旧网 代码更新

---
+++ FeJiu/FeJiu/FeJiu/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FejiuItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    """
+        客户姓名、电话号码、主营业务、地区
+        """
+
+    name = scrapy.Field()
+    number = scrapy.Field()
+    business = scrapy.Field()
+    distract = scrapy.Field()
+
---
+++ FeJiu/FeJiu/FeJiu/spiders/fejiu.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class FejiuSpider(CrawlSpider):
+
+    name = 'fejiu'
+    allowed_domains = ['feijiu.net']
+    start_urls = ['http://www.feijiu.net/FeiZhi/a1g1/']
+
+    rules = (
+        Rule(LinkExtractor(allow=r'aspx/'), callback='parse_item', follow=False),
+        Rule(LinkExtractor(allow=r'ttp://www.feijiu.net/FeiZhi/\w+/'), follow=True),
+    )
+
+    def parse_item(self, response):
+        items = dict()
+        items['name'] = response.xpath('//*[@class="contact"]/div/p/span/text()').extract()[0]
+        items['number'] = response.xpath('//*[@class="contact"]/div/ul/li[1]/img/@src').extract()[0]
+        items['business'] = response.xpath('//*[@id="content"]/div[1]/div[1]/div[2]/ul/li[3]/text()').extract()[0]
+        items['distract'] = response.xpath('//*[@class="contact"]/div/ul/li[4]/text()').extract()[0]
+        print(items)
+        yield items
