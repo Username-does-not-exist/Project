7bfdf9745a8946b72d660050ba31ed91718f5b03
Username-does-not-exist <1432305526@qq.com>
2018-07-13 17:37:01 +0800

    88黄页

---
+++ Waim&Feiz/黄页88/Feiz.py
@@ -0,0 +1,14 @@
+class Feiz(object):
+    def __init__(self):
+        self.base_url = "http://www.huangye88.com/search.html?kw=%E5%BA%9F%E7%BA%B8&"
+
+    def get_detail_url(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    fz = Feiz()
+    fz.main()
---
+++ Waim&Feiz/黄页88/Waimao.py
@@ -0,0 +1,14 @@
+class Waim(object):
+    def __init__(self):
+        self.base_url = "http://www.huangye88.com/search.html?kw=%E5%A4%96%E8%B4%B8%E6%9C%8D%E9%A5%B0&"
+
+    def get_detail_url(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    wm = Waim()
+    wm.main()
