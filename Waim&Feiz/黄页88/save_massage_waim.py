6410c350c2960f69b3b65988fe9615be0fe2a3a4
Username-does-not-exist <1432305526@qq.com>
2018-07-19 10:47:16 +0800

    代码更新

--- Waim&Feiz/黄页88/save_massage_waim.py
+++ Waim&Feiz/黄页88/save_massage_waim.py
@@ -1,17 +1,31 @@
 import re
+import redis
+from selenium import webdriver
+from pymongo import MongoClient
 import sys
 sys.path.append('./')
 from multiprocessing import Process, Queue
 
 
+class Waim(object):
 
-    def get_url( q):
-        urls = rConn.hgetall('url_88wm')
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.Host = "127.0.0.1"
+        self.Port = 27017
+        self.rPort = 6379
+        self.conn = MongoClient(host=self.Host, port=self.Port)
+        self.rConn = redis.Redis(host=self.Host, port=self.rPort)
+
+    def get_url(self, q):
+        urls = self.rConn.hgetall('url_88wm')
         for i in urls:
             url = i.decode('utf-8')
             q.put(url)
 
-    def translate( li, result):
+    def translate(self, li, result):
         """
         解析数据
         :param li:
@@ -47,7 +61,7 @@ from multiprocessing import Process, Queue
             except:
                 pass
 
-    def get_data( q):
+    def get_data(self, q):
         """
         获取数据
         :param url:
@@ -55,48 +69,48 @@ from multiprocessing import Process, Queue
         :return:
         """
         url = q.get()
-        driver.get(url)
-        driver.implicitly_wait(10)
-        button = driver.find_element_by_xpath('//*[@class="meun"]/a[last()]|//*[@class="navigation"]/ul/li[last()]|//*[@class="nav"]/ul/li[last()-1]|//*[@class="vip_nav"]//li[3]')
+        self.driver.get(url)
+        self.driver.implicitly_wait(10)
+        button = self.driver.find_element_by_xpath('//*[@class="meun"]/a[last()]|//*[@class="navigation"]/ul/li[last()]|//*[@class="nav"]/ul/li[last()-1]|//*[@class="vip_nav"]//li[3]')
         button.click()
-        driver.implicitly_wait(5)
-        data_list =start.driver.find_elements_by_xpath('//*[@class="site"]/ul|//*[@class="contact-text"]|//*[@class="address"]/ul|//*[@class="co_Details cf"]')
+        self.driver.implicitly_wait(5)
+        data_list = self.driver.find_elements_by_xpath('//*[@class="site"]/ul|//*[@class="contact-text"]|//*[@class="address"]/ul|//*[@class="co_Details cf"]')
         massage = list()
         for data in data_list:
             item = data.text
             massage.append(item)
 
         items = dict()
-        items['company'] = translate(massage, "公司名称")
-        items['contact'] = translate(massage, '联系人')
-        items['mobile'] = translate(massage, '移动电话')
+        items['company'] = self.translate(massage, "公司名称")
+        items['contact'] = self.translate(massage, '联系人')
+        items['mobile'] = self.translate(massage, '移动电话')
         if items['mobile'] == None:
-            items['mobile'] = translate(massage, "手机")
+            items['mobile'] = self.translate(massage, "手机")
             if items['mobile'] == None:
-                items['mobile'] = translate(massage, "手机号")
+                items['mobile'] = self.translate(massage, "手机号")
 
-        items['number'] = translate(massage, '公司电话')
+        items['number'] = self.translate(massage, '公司电话')
         if items['number'] == None:
-            items['number'] = translate(massage, '电话')
-        items['address'] = translate(massage, '公司地址')
+            items['number'] = self.translate(massage, '电话')
+        items['address'] = self.translate(massage, '公司地址')
         if items['address'] == None:
-            items['address'] = translate(massage, '地址')
-        items['wechat'] = translate(massage, '微信')
+            items['address'] = self.translate(massage, '地址')
+        items['wechat'] = self.translate(massage, '微信')
         if items['wechat'] == None:
-            items['wechat'] = translate(massage, '微信咨询')
-        items['qq'] = translate(massage, 'QQ')
+            items['wechat'] = self.translate(massage, '微信咨询')
+        items['qq'] = self.translate(massage, 'QQ')
         if items['qq'] == None:
-            items['qq'] = translate(massage, "QQ咨询")
-        items['fax'] = translate(massage, '传真')
+            items['qq'] = self.translate(massage, "QQ咨询")
+        items['fax'] = self.translate(massage, '传真')
         if items['fax'] == None:
-            items['fax'] = translate(massage, '公司传真')
-        items['post_number'] = translate(massage, "邮编")
+            items['fax'] = self.translate(massage, '公司传真')
+        items['post_number'] = self.translate(massage, "邮编")
         if items['post_number'] == None:
-            items['post_number'] = translate(massage, "公司邮编")
+            items['post_number'] = self.translate(massage, "公司邮编")
 
         q.put(items)
 
-    def save_data( q):
+    def save_data(self, q):
         """
         保存数据
         :param data:
@@ -104,7 +118,7 @@ from multiprocessing import Process, Queue
         """
         try:
             data = q.get()
-            db = conn.hy88_wm3
+            db = self.conn.hy88_wm3
             col = db.fz
             if data['contact'] == None:
                 pass
@@ -120,9 +134,9 @@ from multiprocessing import Process, Queue
     def main(self):
         q = Queue()
         pool = list()
-        p1 = Process(target=get_url, args=(q,))
-        p2 = Process(target=get_data, args=(q,))
-        p3 = Process(target=save_data, args=(q,))
+        p1 = Process(target=self.get_url, args=(q,))
+        p2 = Process(target=self.get_data, args=(q,))
+        p3 = Process(target=self.save_data, args=(q,))
         pool.append(p1)
         pool.append(p2)
         pool.append(p3)
@@ -131,9 +145,9 @@ from multiprocessing import Process, Queue
             i.join()
 
 
-# if __name__ == '__main__':
-#     w = Waim()
-#     w.main()
+if __name__ == '__main__':
+    w = Waim()
+    w.main()
 
 
 
