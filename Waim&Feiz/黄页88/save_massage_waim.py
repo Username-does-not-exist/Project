0505c48e3a7a6ce142124987a957628f868e0c64
Username-does-not-exist <1432305526@qq.com>
2018-07-19 15:15:53 +0800

    代码更新

--- Waim&Feiz/黄页88/save_massage_waim.py
+++ Waim&Feiz/黄页88/save_massage_waim.py
@@ -1,38 +1,38 @@
 import re
 import redis
+import gevent
+from gevent.queue import Queue
 from selenium import webdriver
 from pymongo import MongoClient
-import sys
-sys.path.append('./')
-from multiprocessing import Process, Queue
 
 
 class Waim(object):
 
-    def __init__(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        self.Host = "127.0.0.1"
-        self.Port = 27017
-        self.rPort = 6379
-        self.conn = MongoClient(host=self.Host, port=self.Port)
-        self.rConn = redis.Redis(host=self.Host, port=self.rPort)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    Host = "127.0.0.1"
+    Port = 27017
+    rPort = 6379
+    conn = MongoClient(host=Host, port=Port)
+    rConn = redis.Redis(host=Host, port=rPort)
 
+    def __init__(self):
+        pass
+
+    @classmethod
     def get_url(self, q):
         urls = self.rConn.hgetall('url_88wm')
-        for i in urls:
-            url = i.decode('utf-8')
-            q.put(url)
+        q.put(urls)
 
-    def translate(self, li, result):
+    @staticmethod
+    def translate(li, result):
         """
         解析数据
         :param li:
         :param result:
         :return:
         """
-
         a = str(li)
         a_str = a.replace('[', '').replace(']', '').replace("'", '')
 
@@ -58,9 +58,11 @@ class Waim(object):
                 num = item2 + 1
                 company = c_list[num]
                 return company
-            except:
+            except Exception as e:
                 pass
 
+
+    @classmethod
     def get_data(self, q):
         """
         获取数据
@@ -68,86 +70,60 @@ class Waim(object):
         :param proxy:
         :return:
         """
-        url = q.get()
-        self.driver.get(url)
-        self.driver.implicitly_wait(10)
-        button = self.driver.find_element_by_xpath('//*[@class="meun"]/a[last()]|//*[@class="navigation"]/ul/li[last()]|//*[@class="nav"]/ul/li[last()-1]|//*[@class="vip_nav"]//li[3]')
-        button.click()
-        self.driver.implicitly_wait(5)
-        data_list = self.driver.find_elements_by_xpath('//*[@class="site"]/ul|//*[@class="contact-text"]|//*[@class="address"]/ul|//*[@class="co_Details cf"]')
-        massage = list()
-        for data in data_list:
-            item = data.text
-            massage.append(item)
+        urls = q.get()
+        for i in urls:
+            url = i.decode("utf-8")
+            self.driver.get(url)
+            self.driver.implicitly_wait(10)
+            button = self.driver.find_element_by_xpath('//*[@class="meun"]/a[last()]|//*[@class="navigation"]/ul/li[last()]|//*[@class="nav"]/ul/li[last()-1]|//*[@class="vip_nav"]//li[3]')
+            button.click()
+            self.driver.implicitly_wait(5)
+            data_list = self.driver.find_elements_by_xpath('//*[@class="site"]/ul|//*[@class="contact-text"]|//*[@class="address"]/ul|//*[@class="co_Details cf"]')
+            massage = list()
+            for data in data_list:
+                item = data.text
+                massage.append(item)
 
-        items = dict()
-        items['company'] = self.translate(massage, "公司名称")
-        items['contact'] = self.translate(massage, '联系人')
-        items['mobile'] = self.translate(massage, '移动电话')
-        if items['mobile'] == None:
-            items['mobile'] = self.translate(massage, "手机")
+            items = dict()
+            items['company'] = self.translate(massage, "公司名称")
+            items['contact'] = self.translate(massage, '联系人')
+            items['mobile'] = self.translate(massage, '移动电话')
             if items['mobile'] == None:
-                items['mobile'] = self.translate(massage, "手机号")
+                items['mobile'] = self.translate(massage, "手机")
+                if items['mobile'] == None:
+                    items['mobile'] = self.translate(massage, "手机号")
+            items['number'] = self.translate(massage, '公司电话')
+            if items['number'] == None:
+                items['number'] = self.translate(massage, '电话')
+            items['address'] = self.translate(massage, '公司地址')
+            if items['address'] == None:
+                items['address'] = self.translate(massage, '地址')
+            items['wechat'] = self.translate(massage, '微信')
+            if items['wechat'] == None:
+                items['wechat'] = self.translate(massage, '微信咨询')
+            items['qq'] = self.translate(massage, 'QQ')
+            if items['qq'] == None:
+                items['qq'] = self.translate(massage, "QQ咨询")
+            items['fax'] = self.translate(massage, '传真')
+            if items['fax'] == None:
+                items['fax'] = self.translate(massage, '公司传真')
+            items['post_number'] = self.translate(massage, "邮编")
+            if items['post_number'] == None:
+                items['post_number'] = self.translate(massage, "公司邮编")
 
-        items['number'] = self.translate(massage, '公司电话')
-        if items['number'] == None:
-            items['number'] = self.translate(massage, '电话')
-        items['address'] = self.translate(massage, '公司地址')
-        if items['address'] == None:
-            items['address'] = self.translate(massage, '地址')
-        items['wechat'] = self.translate(massage, '微信')
-        if items['wechat'] == None:
-            items['wechat'] = self.translate(massage, '微信咨询')
-        items['qq'] = self.translate(massage, 'QQ')
-        if items['qq'] == None:
-            items['qq'] = self.translate(massage, "QQ咨询")
-        items['fax'] = self.translate(massage, '传真')
-        if items['fax'] == None:
-            items['fax'] = self.translate(massage, '公司传真')
-        items['post_number'] = self.translate(massage, "邮编")
-        if items['post_number'] == None:
-            items['post_number'] = self.translate(massage, "公司邮编")
-
-        q.put(items)
-
-    def save_data(self, q):
-        """
-        保存数据
-        :param data:
-        :return:
-        """
-        try:
-            data = q.get()
-            db = self.conn.hy88_wm3
-            col = db.fz
-            if data['contact'] == None:
-                pass
-            else:
-                col.insert(data)
-                count = col.count()
-                print(data)
-                print("<|---------------=================----------------|>")
-                print("当前已抓取{}条数据".format(count))
-        except Exception as e:
-            print(e)
-
-    def main(self):
-        q = Queue()
-        pool = list()
-        p1 = Process(target=self.get_url, args=(q,))
-        p2 = Process(target=self.get_data, args=(q,))
-        p3 = Process(target=self.save_data, args=(q,))
-        pool.append(p1)
-        pool.append(p2)
-        pool.append(p3)
-        for i in pool:
-            i.start()
-            i.join()
+            db = self.conn.hy88
+            col = db.wm
+            col.insert(items)
+            print(items)
 
 
 if __name__ == '__main__':
-    w = Waim()
-    w.main()
+    q = Queue()
+    pool = list()
 
-
+    wm = Waim()
+    gevent.joinall([
+        gevent.spawn(wm.get_url, q),
+        gevent.spawn(wm.get_data, q)
+    ])
 
