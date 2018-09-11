5e610c319141f6f9e9afbd281849ebc42410db57
敖国法 <aogf@asto-inc.com>
2018-09-11 15:44:01 +0800

    基本抓取思路

--- September/fengj.com/CrawlMessage.py
+++ September/fengj.com/CrawlMessage.py
@@ -1,5 +1,10 @@
 import redis
+import requests
+from lxml import etree
 from pymongo import MongoClient
+import sys
+sys.path.append('../')
+from UserAgentPool import UAPool
 
 
 class CrawlMessage(object):
@@ -9,19 +14,41 @@ class CrawlMessage(object):
         rport = "6379"
         self.RedisClint = redis.Redis(host=rhost, port=rport)
         mhost = '127.0.0.1'
-        mport = '27017'
+        mport = 27017
         Client = MongoClient(host=mhost, port=mport)
         self.db = Client.gongqiu
         self.collection = self.db.gq
 
-    def get_data(self):
-        pass
+    def construct_headers(self, url):
+        header = {
+            "Referer": "http://linsen.fengj.com/",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": UAPool().get()
+        }
+        return header
 
-    def save_data(self):
+    def get_data(self, contact_url, header):
+        response = requests.get(contact_url, headers=header)
+        page = response.text
+        html = etree.HTML(page)
+        items = dict()
+        items[''] = html.xpath('')
+        items[''] = html.xpath('')
+        items[''] = html.xpath('')
+        items[''] = html.xpath('')
+        return items
+
+    def save_data(self, items):
         pass
 
     def main(self):
-        pass
+        url_list = self.RedisClint.hgetall('fengj_gongqiu')
+        for i in url_list:
+            url = i.decode("utf-8")
+            contact_url = url + "/lxfs.html"
+            header = self.construct_headers(url)
+            items = self.get_data(contact_url, header)
+            self.save_data(items)
 
 
 if __name__ == '__main__':
