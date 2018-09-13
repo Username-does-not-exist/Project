6a3b77437888e97e28acf5db929d054b94263ad2
敖国法 <aogf@asto-inc.com>
2018-09-13 14:05:30 +0800

    保存数据

--- September/feijiu.net/Crawl.py
+++ September/feijiu.net/Crawl.py
@@ -5,10 +5,17 @@
 
 
 """
+import os
+import json
 import redis
+import requests
+from lxml import etree
 from pymongo import MongoClient
 from selenium import webdriver
+from PIL import Image
+from io import BytesIO
 from selenium.webdriver import ActionChains
+from requests.cookies import RequestsCookieJar
 
 
 class Crawl(object):
@@ -28,6 +35,32 @@ class Crawl(object):
         self.db = client.FJgy
         self.collection = self.db.gy
 
+    def login_and_cookies(self):
+        """
+        处理登陆逻辑
+        :return:
+        """
+        # 登陆
+        self.driver.get(self.login_url)
+        self.driver.implicitly_wait(5)
+        # 输入账号
+        username = self.driver.find_element_by_xpath('//*[@id="user"]')
+        ActionChains(self.driver).move_to_element(username)
+        username.click()
+        username.send_keys("mixiu")
+        self.driver.implicitly_wait(10)
+        # 输入密码
+        password = self.driver.find_element_by_xpath('//*[@id="psw"]')
+        ActionChains(self.driver).move_to_element(password)
+        password.send_keys('fangfei930916.+')
+        # 点击登陆
+        login_button = self.driver.find_element_by_xpath('//*[@id="btnUSubmit"]')
+        login_button.click()
+        self.driver.implicitly_wait(5)
+        cookies = self.driver.get_cookies()
+        with open('cookies.txt', 'w') as file:
+            json.dump(cookies, file)
+
     def construct_url(self):
         url_list = []
         for i in range(1, 20):
@@ -41,60 +74,64 @@ class Crawl(object):
         :param url:
         :return:
         """
+        url = ""
+        return url
 
-    def login_and_cookies(self):
-        """
-        处理登陆逻辑
-        :return:
-        """
-        # 登陆
-        self.driver.get(self.login_url)
-        self.driver.implicitly_wait(5)
-        # 输入账号
-        username = self.driver.find_element_by_xpath('//*[@id="txt_uname"]')
-        ActionChains(self.driver).move_to_element(username)
-        username.click()
-        username.send_keys("zhuzhu1991")
-        self.driver.implicitly_wait(10)
-        # 输入密码
-        password = self.driver.find_element_by_xpath('//*[@id="txt_pass"]')
-        ActionChains(self.driver).move_to_element(password)
-        password.send_keys('zhu741852')
-        # 点击登陆
-        login_button = self.driver.find_element_by_xpath('//*[@id="btn1"]')
-        login_button.click()
-        self.driver.implicitly_wait(5)
-        cookies = self.driver.get_cookies()
-        return cookies
-    
-    def get_contact_info(self, url, cookies):
+    def get_contact_info(self, url):
         """
         获取公司信息
         :param url:
         :param cookies:
         :return:
         """
+        session = requests.session()
+        session.verify = False
+        session.headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36"
+        }
+        jar = RequestsCookieJar()
+        with open('cookies.txt', 'r') as file:
+            cookies = json.load(file)
+            for cookie in cookies:
+                jar.set(cookie['name'], cookie['value'])
+        response = session.get(url)
+        page = response.text
+        html = etree.HTML(page)
+        items = html.xpath('//*[@class="contact"]//text()')
+        contact_info_picture_url = html.xpath('//*[@class="contact"]/div/ul/li/img/@src')
+        return items, contact_info_picture_url
 
-    def save_data(self, data, contact_info_picture):
+    def save_data(self, data, contact_info_picture_url):
         """
         保存数据
         :param data:
-        :param contact_info_picture:
+        :param contact_info_picture_url:
         :return:
         """
+        company = data['company']
+        path = os.getcwd()
+        folder = path + "\\Image_GY"
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        if len(contact_info_picture_url) >= 1:
+            url = contact_info_picture_url[0]
+            response = requests.get(url)
+            image = Image.open(BytesIO(response.content))
+            image.save(folder + '/{}.png'.format(company))
+        self.collection.insert(data)
 
     def main(self):
         """
         处理抓取逻辑
         :return:
         """
-        cookies =  self.login_and_cookies()
+        self.login_and_cookies()
         url_list = self.construct_url()
         for url in url_list:
             company_url_list = self.get_company_url(url)
             for company_url in company_url_list:
-                company_info, contact_info_picture = self.get_contact_info(company_url, cookies)
-                self.save_data(company_info, contact_info_picture)
+                company_info, contact_info_picture_url = self.get_contact_info(company_url)
+                self.save_data(company_info, contact_info_picture_url)
 
 
 if __name__ == '__main__':
